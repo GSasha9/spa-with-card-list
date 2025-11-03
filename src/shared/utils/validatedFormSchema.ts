9051e80be36ca72f211daf27ac1d93b99c72b7d5
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-11-03 12:31:20 +0300

    feat: implement opening of created card on detail page

--- src/shared/utils/validatedFormSchema.ts
+++ src/shared/utils/validatedFormSchema.ts
@@ -12,8 +12,12 @@ export const validatedFormSchema = z.object({
   status: z.enum(['Alive', 'Dead', 'Unknown']),
 
   species: z.string().min(1, FORM_ERROR_MESSAGES.name.required),
-  location: z.string().min(1, FORM_ERROR_MESSAGES.name.required),
-  lastLocation: z.string().min(1, FORM_ERROR_MESSAGES.name.required),
+  location: z.object({
+    name: z.string().min(1, FORM_ERROR_MESSAGES.name.required),
+  }),
+  origin: z.object({
+    name: z.string().min(1, FORM_ERROR_MESSAGES.name.required),
+  }),
   image: z
     .string()
     .refine((val) => val.length > 0, FORM_ERROR_MESSAGES.file.required)
